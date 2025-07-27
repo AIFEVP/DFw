d3b74dc8e433e26d78480a93851020ffa1a174a3
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-07-27 13:25:39 -0700

    nested html

--- electronFw/eApps/sampleApp/main.js
+++ electronFw/eApps/sampleApp/main.js
@@ -7,12 +7,28 @@ function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        frame: false,
+        backgroundColor: '#6f1818',
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
         },
     });
+
+
+    const nested = new BrowserWindow({
+        width: 800,
+        height: 900,
+        // frame: false,
+        backgroundColor: '#31778fff',
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
     win.loadFile(path.join(__dirname, 'indexentry.html'));
+    nested.loadFile(path.join(__dirname, 'nestedyash.html'));
     win.webContents.openDevTools();
 }
 
