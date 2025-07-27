5167472a0942ba5b5f5400ad84fcfe4af866098c
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-07-27 00:20:22 -0700

    electron load file

--- electronFw/eApps/sampleApp/main.js
+++ electronFw/eApps/sampleApp/main.js
@@ -1,1 +1,18 @@
 console.info(`welcome to electron!`);
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+    win.loadFile(path.join(__dirname, 'indexentry.html'));
+}
+
+app.whenReady().then(createWindow);
