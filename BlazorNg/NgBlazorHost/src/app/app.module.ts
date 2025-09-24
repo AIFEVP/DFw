558f8c3a496035a59b516102a18ace0c6fa7a87e
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-09-23 23:33:25 -0700

    ng host

---
+++ BlazorNg/NgBlazorHost/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA ]
+})
+export class AppModule { }
