1fcbcd2d71d1ad58b19d8cbb83f6164eb248422a
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-11-13 01:12:19 -0800

    aspire deploy; aspire add docker; localhost:56454

--- Aspire/Aspire.AppHost/AppHost.cs
+++ Aspire/Aspire.AppHost/AppHost.cs
@@ -1,14 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 // Add the Docker Compose environment
-builder.AddDockerComposeEnvironment("composedir");
+builder.AddDockerComposeEnvironment("env");
+
+// Add a Redis server to the application and set lifetime to persistent
+var cache = builder.AddRedis("cache")
+                   .WithLifetime(ContainerLifetime.Persistent);
 
 var apiService = builder.AddProject<Projects.Aspire_ApiService>("apiservice")
-    .WithHttpHealthCheck("/health");
+    .WithHttpHealthCheck("/health")
+    .WaitFor(cache);
 
 builder.AddProject<Projects.Aspire_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithHttpHealthCheck("/health")
     .WithReference(apiService)
-    .WaitFor(apiService);
+    .WaitFor(apiService)
+    .WithReference(cache)
+    .WaitFor(cache);
 
 builder.Build().Run();
